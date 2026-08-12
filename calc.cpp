7575caf859d23bf122189dad597dab32e39f7780
Python-Wizard11 <rmsaini_b25@it.vjti.ac.in>
2026-08-13 01:31:28 +0530

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    cout << "Their sum is " << a + b << endl;
+    cout << "Their difference is "<< a - b << endl;
+
+
+    return 0;
+}
